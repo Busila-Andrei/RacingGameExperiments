e120bbac40ecb9169b8f4e80861b35f3899a5218
Teeto-code-java <andreibusila9819@gmail.com>
2020-09-19 03:38:39 -0700

    studied has-a relationship

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -23,13 +23,34 @@ public class App
         System.out.println("Door Count: " + carReference.doorCount);
         System.out.println("Color: " + carReference.color);
         System.out.println(carReference);
+        System.out.println();
+
+        Engine engine1 = new Engine();
+        engine1.manufacturer = "Ferrari";
+        engine1.capacity = 3000;
+
+        carReference.engine = engine1;
+
+        System.out.println("Engine1 capacity: " + engine1.capacity);
+        System.out.println("Car engine capacity: " +carReference.engine.capacity);
+
+        engine1.capacity = 4000;
+
+        System.out.println("Car engine capacity after update: " +carReference.engine.capacity);
+
+
 
         System.out.println();
 
+        // concatenation
         Car car2 = new Car();
         car2.name = "Renault";
         car2.fuelLevel = 70;
         car2.totalTraveledDistance = 100;
+
+        car2.engine = new Engine();
+        car2.engine.capacity = 2000;
+
         System.out.println("Properties of car " + car2.name);
         System.out.println("Max speed: " + car2.maxSpeed);
         System.out.println("Fuel level: " + car2.fuelLevel);
@@ -40,7 +61,7 @@ public class App
         System.out.println("Color: " + car2.color);
         System.out.println(car2);
 
-        System.out.println("Initial name for car1" + carReference.name);
+        System.out.println("Initial name for car1 " + carReference.name);
 
         carReference.name = "Audi";
 
